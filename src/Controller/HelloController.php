edc7be1d6321a1199b430ff2f277585109bacc66
yunusyerli1 <yunusyerli1@gmail.com>
2023-07-16 14:26:11 +0300

    Create User, Comment, and DB Relations

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,9 +1,17 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\User;
+use App\Entity\Comment;
+use App\Entity\MicroPost;
+use App\Entity\UserProfile;
+use App\Repository\CommentRepository;
+use App\Repository\MicroPostRepository;
+use App\Repository\UserProfileRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloController extends AbstractController
 {
@@ -13,15 +21,36 @@ class HelloController extends AbstractController
         ['message' => 'Bye!', 'created' => '2021/05/12']
     ];
 
-    #[Route('/{limit<\d+>?3}', name: 'app_index')]
-    public function index(int $limit): Response
+    #[Route('/', name: 'app_index')]
+    public function index(MicroPostRepository $posts, CommentRepository $comments): Response
     {
-        //return new Response(implode(',', array_slice($this->messages, 0, $limit)));
+        // $user = new User();
+        // $user->setEmail('email1@email.com');
+        // $user->setPassword('12345678');
+
+        // $profile = new UserProfile();
+        // $profile->setUser($user);
+        // $profiles->add($profile, true);
+
+         $post = new MicroPost();
+         $post->setTitle('Hello');
+         $post->setText('Hello');
+         $post->setDate(new DateTime());
+
+        
+
+        $comment = new Comment();
+        $comment->setText('Hello');
+        $post->addComment($comment);
+        $posts->add($post, true);
+        //  $comment = $post->getComments()[0];
+        //  $comment->setPost(null);
+        //  $comments->add($comment, true);
+
         return $this->render(
             'hello/index.html.twig',
             [
-                'messages' => $this->messages,
-                'limit' => $limit
+                'messages' => $this->messages
             ]
         );
     }
--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MicroPostRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -27,6 +29,14 @@ class MicroPost
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $date = null;
+
+    #[ORM\OneToMany(mappedBy: 'post', targetEntity: Comment::class, orphanRemoval: true)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -65,6 +75,36 @@ class MicroPost
     public function setDate(\DateTimeInterface $date): static
     {
         $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getPost() === $this) {
+                $comment->setPost(null);
+            }
+        }
 
         return $this;
     }
